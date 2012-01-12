2f4e3d7ad43a9db29261d183070f3e92495b9fe1
Tomas Mlcoch <tmlcoch@redhat.com>
2012-01-12 10:26:29 +0100

    Add constants.h

---
+++ constants.h
@@ -0,0 +1,10 @@
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+
+typedef enum {
+    PKG_CHECKSUM_MD5,
+    PKG_CHECKSUM_SHA1,
+    PKG_CHECKSUM_SHA256
+} ChecksumType;
+
+#endif /* __CONSTANTS__ */

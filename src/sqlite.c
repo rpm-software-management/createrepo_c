827fbd47eca0d3174ee595bf2c3c03fb083728d5
Tomas Mlcoch <tmlcoch@redhat.com>
2012-07-03 12:50:31 +0200

    Add authors of db.c from yum-metadata-parser into the sqlite module.

--- src/sqlite.c
+++ src/sqlite.c
@@ -1,5 +1,10 @@
 /* createrepo_c - Library of routines for manipulation with repodata
- * Copyright (C) 2012  Tomas Mlcoch
+ * Copyright (C) 2012      Tomas Mlcoch
+ * Copyright (C) 2008-2011 James Antill
+ * Copyright (C) 2006-2007 James Bowes
+ * Copyright (C) 2006-2007 Paul Nasrat
+ * Copyright (C) 2006      Tambet Ingo
+ * Copyright (C) 2006-2010 Seth Vidal
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,8 +18,15 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  */
+
+/*
+TODO:
+- Fix possible license issue of this module!!!
+  - Relicensins whole project to GPLv2?
+  - Rewrite sqlite module?
+*/
 
 #include <glib.h>
 #include <string.h>

6a2c3b4c65160469e2ed658e19dc99e72d6444b4
Tomas Mlcoch <tmlcoch@redhat.com>
2013-02-11 09:06:30 +0100

    Bump to 0.1.15

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ extern "C" {
 /**@{*/
 #define CR_MAJOR_VERSION   0    /*!< major library version */
 #define CR_MINOR_VERSION   1    /*!< minor library version */
-#define CR_PATCH_VERSION   14   /*!< patch library version */
+#define CR_PATCH_VERSION   15   /*!< patch library version */
 /**@}*/
 
 #ifdef __cplusplus

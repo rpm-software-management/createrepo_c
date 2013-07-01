6c9f215dab93011705c59b2f626e1854a1950986
Tomas Mlcoch <tmlcoch@redhat.com>
2013-07-01 17:04:24 +0200

    tests: Use assertTrue(isinstance..  instead of assertIsInstance( (For comptatibility reasons)

--- tests/python/tests/test_version.py
+++ tests/python/tests/test_version.py
@@ -5,6 +5,6 @@ import fixtures
 
 class TestCaseVersion(unittest.TestCase):
     def test_version(self):
-        self.assertIsInstance(cr.VERSION_MAJOR, int);
-        self.assertIsInstance(cr.VERSION_MINOR, int);
-        self.assertIsInstance(cr.VERSION_PATCH, int);
+        self.assertTrue(isinstance(cr.VERSION_MAJOR, int));
+        self.assertTrue(isinstance(cr.VERSION_MINOR, int));
+        self.assertTrue(isinstance(cr.VERSION_PATCH, int));

d2cab19298bfd1b265174c871e8fe8f085292bb8
Tomas Mlcoch <tmlcoch@redhat.com>
2014-02-25 17:58:52 +0100

    utils: Update make_rpm.sh script

--- utils/make_rpm.sh
+++ utils/make_rpm.sh
@@ -2,26 +2,18 @@
 
 RPMBUILD_DIR="${HOME}/rpmbuild/"
 BUILD_DIR="$RPMBUILD_DIR/BUILD"
-TARGETDIR=`pwd -P`
-SCRIPTDIR=$( cd "$(dirname "$0")" ; pwd -P )
-PROJECTROOTDIR=`realpath "$SCRIPTDIR/../"`
 GITREV=`git rev-parse --short HEAD`
+PREFIX=""   # Root project dir
+MY_DIR=`dirname $0`
+MY_DIR="$MY_DIR/"
 
-echo "Cleaning $BUILD_DIR"
-rm -rf $BUILD_DIR
-echo "Removing $RPMBUILD_DIR/createrepo_c.spec"
-rm -f $RPMBUILD_DIR/createrepo_c.spec
-
-if [ $# -gt "1" ] && [ "$1" = "-h" ]; then
-    echo "Usage: `basename $0` [git revision]"
-    exit 0
+if [ $# -lt "1"  -o $# -gt "2" ]
+then
+    echo "Usage: `basename $0` <root_project_dir> [revision]"
+    exit 1
 fi
 
-if [ $# -eq "1" ]; then
-    GITREV="$1"
-fi
-
-echo "Generating rpm for $GITREV"
+PREFIX="$1/"
 
 if [ ! -d "$RPMBUILD_DIR" ]; then
     echo "rpmbuild dir $RPMBUILD_DIR doesn't exist!"
@@ -30,9 +22,15 @@ if [ ! -d "$RPMBUILD_DIR" ]; then
     exit 1
 fi
 
-echo "> Making tarball .."
-$SCRIPTDIR/make_tarball.sh $GITREV
+echo "Generating rpm for $GITREV"
 
+echo "Cleaning $BUILD_DIR"
+rm -rf $BUILD_DIR
+echo "Removing $RPMBUILD_DIR/createrepo_c.spec"
+rm -f $RPMBUILD_DIR/createrepo_c.spec
+
+echo "> Making tarball .."
+$MY_DIR/make_tarball.sh $GITREV
 if [ ! $? == "0" ]; then
     echo "Error while making tarball"
     exit 1
@@ -40,13 +38,15 @@ fi
 echo "Tarball done"
 
 echo "> Copying tarball and .spec file into the $RPMBUILD_DIR .."
-cp createrepo_c-$GITREV.tar.xz $RPMBUILD_DIR/SOURCES/
+cp $PREFIX/createrepo_c-$GITREV.tar.xz $RPMBUILD_DIR/SOURCES/
 if [ ! $? == "0" ]; then
     echo "Error while: cp createrepo_c-*.tar.xz $RPMBUILD_DIR/SOURCES/"
     exit 1
 fi
 
-cp $PROJECTROOTDIR/createrepo_c.spec $RPMBUILD_DIR/SPECS/
+# Copy via sed
+sed -i "s/%global gitrev .*/%global gitrev $GITREV/g" $PREFIX/createrepo_c.spec
+sed "s/%global gitrev .*/%global gitrev $GITREV/g" $PREFIX/createrepo_c.spec > $RPMBUILD_DIR/SPECS/createrepo_c.spec
 if [ ! $? == "0" ]; then
     echo "Error while: cp $PROJECTROOTDIR/createrepo_c*.spec $RPMBUILD_DIR/SPECS/"
     exit 1
@@ -66,11 +66,11 @@ rpmbuild --clean $RPMBUILD_DIR/SPECS/createrepo_c.spec
 echo "Cleanup done"
 
 echo "> Moving rpms and srpm .."
-mv --verbose $RPMBUILD_DIR/SRPMS/createrepo_c-*.src.rpm $TARGETDIR/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/createrepo_c-*.rpm $TARGETDIR/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/python-createrepo_c-*.rpm $TARGETDIR/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/python-deltarepo-*.rpm $TARGETDIR/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/deltarepo-*.rpm $TARGETDIR/.
+mv --verbose $RPMBUILD_DIR/SRPMS/createrepo_c-*.src.rpm $PREFIX/.
+mv --verbose $RPMBUILD_DIR/RPMS/*/createrepo_c-*.rpm $PREFIX/.
+mv --verbose $RPMBUILD_DIR/RPMS/*/python-createrepo_c-*.rpm $PREFIX/.
+mv --verbose $RPMBUILD_DIR/RPMS/*/python-deltarepo-*.rpm $PREFIX/.
+mv --verbose $RPMBUILD_DIR/RPMS/*/deltarepo-*.rpm $PREFIX/.
 echo "Moving done"
 
 echo "All done!"

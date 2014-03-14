da76017b979253b57c717b443df1351ccafa634a
Tomas Mlcoch <xtojaj@gmail.com>
2014-03-14 10:18:29 +0100

    Update utility scripts

--- utils/make_rpm.sh
+++ utils/make_rpm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+PACKAGE="createrepo_c"
 RPMBUILD_DIR="${HOME}/rpmbuild/"
 BUILD_DIR="$RPMBUILD_DIR/BUILD"
 GITREV=`git rev-parse --short HEAD`
 PREFIX=""   # Root project dir
-MY_DIR=`dirname $0`
-MY_DIR="$MY_DIR/"
+MY_DIR=`dirname "$0"`
 
 if [ $# -lt "1"  -o $# -gt "2" ]
 then
-    echo "Usage: `basename $0` <root_project_dir> [revision]"
+    echo "Usage: `basename "$0"` <root_project_dir> [revision]"
     exit 1
 fi
 
@@ -25,12 +25,12 @@ fi
 echo "Generating rpm for $GITREV"
 
 echo "Cleaning $BUILD_DIR"
-rm -rf $BUILD_DIR
-echo "Removing $RPMBUILD_DIR/createrepo_c.spec"
-rm -f $RPMBUILD_DIR/createrepo_c.spec
+rm -rf "$BUILD_DIR"
+echo "Removing $RPMBUILD_DIR/$PACKAGE.spec"
+rm -f "$RPMBUILD_DIR/$PACKAGE.spec"
 
 echo "> Making tarball .."
-$MY_DIR/make_tarball.sh $GITREV
+"$MY_DIR/make_tarball.sh" "$GITREV"
 if [ ! $? == "0" ]; then
     echo "Error while making tarball"
     exit 1
@@ -38,39 +38,37 @@ fi
 echo "Tarball done"
 
 echo "> Copying tarball and .spec file into the $RPMBUILD_DIR .."
-cp $PREFIX/createrepo_c-$GITREV.tar.xz $RPMBUILD_DIR/SOURCES/
+cp "$PREFIX/$PACKAGE-$GITREV.tar.xz" "$RPMBUILD_DIR/SOURCES/"
 if [ ! $? == "0" ]; then
-    echo "Error while: cp createrepo_c-*.tar.xz $RPMBUILD_DIR/SOURCES/"
+    echo "Error while: cp $PREFIX/$PACKAGE-$GITREV.tar.xz $RPMBUILD_DIR/SOURCES/"
     exit 1
 fi
 
 # Copy via sed
-sed -i "s/%global gitrev .*/%global gitrev $GITREV/g" $PREFIX/createrepo_c.spec
-sed "s/%global gitrev .*/%global gitrev $GITREV/g" $PREFIX/createrepo_c.spec > $RPMBUILD_DIR/SPECS/createrepo_c.spec
+sed -i "s/%global gitrev .*/%global gitrev $GITREV/g" "$PREFIX/$PACKAGE.spec"
+sed "s/%global gitrev .*/%global gitrev $GITREV/g" "$PREFIX/$PACKAGE.spec" > "$RPMBUILD_DIR/SPECS/$PACKAGE.spec"
 if [ ! $? == "0" ]; then
-    echo "Error while: cp $PROJECTROOTDIR/createrepo_c*.spec $RPMBUILD_DIR/SPECS/"
+    echo "Error while: cp $PREFIX/$PACKAGE.spec $RPMBUILD_DIR/SPECS/"
     exit 1
 fi
 echo "Copying done"
 
-echo "> Starting rpmbuild createrepo_c.."
-rpmbuild -ba $RPMBUILD_DIR/SPECS/createrepo_c.spec
+echo "> Starting rpmbuild $PACKAGE.."
+rpmbuild -ba "$RPMBUILD_DIR/SPECS/$PACKAGE.spec"
 if [ ! $? == "0" ]; then
-    echo "Error while: rpmbuild -ba $RPMBUILD_DIR/SPECS/createrepo_c.spec"
+    echo "Error while: rpmbuild -ba $RPMBUILD_DIR/SPECS/$PACKAGE.spec"
     exit 1
 fi
 echo "rpmbuild done"
 
 echo "> Cleanup .."
-rpmbuild --clean $RPMBUILD_DIR/SPECS/createrepo_c.spec
+rpmbuild --clean "$RPMBUILD_DIR/SPECS/$PACKAGE.spec"
 echo "Cleanup done"
 
 echo "> Moving rpms and srpm .."
-mv --verbose $RPMBUILD_DIR/SRPMS/createrepo_c-*.src.rpm $PREFIX/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/createrepo_c-*.rpm $PREFIX/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/python-createrepo_c-*.rpm $PREFIX/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/python-deltarepo-*.rpm $PREFIX/.
-mv --verbose $RPMBUILD_DIR/RPMS/*/deltarepo-*.rpm $PREFIX/.
+mv --verbose "$RPMBUILD_DIR"/SRPMS/"$PACKAGE"-*.src.rpm "$PREFIX/."
+mv --verbose "$RPMBUILD_DIR"/RPMS/*/"$PACKAGE"-*.rpm "$PREFIX/."
+mv --verbose "$RPMBUILD_DIR"/RPMS/*/python*-"$PACKAGE"-*.rpm "$PREFIX/."
 echo "Moving done"
 
 echo "All done!"
--- utils/make_tarball.sh
+++ utils/make_tarball.sh
@@ -1,11 +1,12 @@
+PACKAGE="createrepo_c"
 TARGET_DIR="./"
 
 if [ "$#" -eq "0" ]; then
     GITREV=`git rev-parse --short HEAD`
 else
-    GITREV=$1
+    GITREV="$1"
 fi
 
 echo "Generate tarball for revision: $GITREV"
 
-git archive ${GITREV} --prefix=createrepo_c/ | xz > $TARGET_DIR/createrepo_c-${GITREV}.tar.xz
+git archive "${GITREV}" --prefix="$PACKAGE"/ | xz > "$TARGET_DIR"/"$PACKAGE"-"${GITREV}".tar.xz

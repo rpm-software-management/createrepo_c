eb61dac3ca96f4bc240431011756397f9217c9cf
Tomas Mlcoch <tmlcoch@redhat.com>
2016-09-08 09:48:18 +0200

    Update cleanup script to remove also packages for Python 3

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -fv createrepo_c-*.tar.xz
-rm -fv createrepo_c-*.rpm python-createrepo_c-*.rpm
+rm -fv createrepo_c-*.rpm python-createrepo_c-*.rpm python3-createrepo_c-*.rpm
 rm -fv deltarepo-*.rpm python-deltarepo-*.rpm

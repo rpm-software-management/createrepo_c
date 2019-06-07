4da02ef597ef4f940331ed00b91da4a299f64813
Aleš Matěj <amatej@redhat.com>
2019-06-07 13:26:41 +0200

    Add tests for checking if package contains forbidden control chars

--- tests/test_xml_dump.c
+++ tests/test_xml_dump.c
@@ -28,6 +28,71 @@ test_cr_prepend_protocol_01(void)
     g_free(prepended_url);
 }
 
+static void
+test_cr_Package_contains_forbidden_control_chars_01(void)
+{
+    cr_Package *p = get_package();
+    g_assert(!cr_Package_contains_forbidden_control_chars(p));
+}
+
+static void
+test_cr_Package_contains_forbidden_control_chars_02(void)
+{
+    cr_Package *p = get_package();
+    p->name = "foo";
+
+    g_assert(cr_Package_contains_forbidden_control_chars(p));
+}
+
+static void
+test_cr_Package_contains_forbidden_control_chars_03(void)
+{
+    cr_Package *p = get_package();
+    p->summary = "foo";
+
+    g_assert(cr_Package_contains_forbidden_control_chars(p));
+}
+
+static void
+test_cr_Package_contains_forbidden_control_chars_04(void)
+{
+    cr_Package *p = get_package();
+    cr_Dependency *dep = p->requires->data;
+    dep->name = "foobar_dep";
+
+    g_assert(cr_Package_contains_forbidden_control_chars(p));
+}
+
+static void
+test_cr_Package_contains_forbidden_control_chars_05(void)
+{
+    cr_Package *p = get_package();
+    cr_PackageFile *file = p->files->data;
+    file->name = "obar_dep";
+
+    g_assert(cr_Package_contains_forbidden_control_chars(p));
+}
+
+static void
+test_cr_GSList_of_cr_Dependency_contains_forbidden_control_chars_01(void)
+{
+    cr_Package *p = get_package();
+    cr_Dependency *dep = p->requires->data;
+    dep->name = "foobar_dep";
+
+    g_assert(cr_GSList_of_cr_Dependency_contains_forbidden_control_chars(p->requires));
+}
+
+static void
+test_cr_GSList_of_cr_Dependency_contains_forbidden_control_chars_02(void)
+{
+    cr_Package *p = get_package();
+    cr_Dependency *dep = p->requires->data;
+    dep->name = "fo	badep";
+
+    g_assert(!cr_GSList_of_cr_Dependency_contains_forbidden_control_chars(p->requires));
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -35,8 +100,22 @@ main(int argc, char *argv[])
 
     g_test_add_func("/xml_dump/test_cr_prepend_protocol_00",
                     test_cr_prepend_protocol_00);
-
     g_test_add_func("/xml_dump/test_cr_prepend_protocol_01",
                     test_cr_prepend_protocol_01);
+
+    g_test_add_func("/xml_dump/test_cr_Package_contains_forbidden_control_chars_01",
+                    test_cr_Package_contains_forbidden_control_chars_01);
+    g_test_add_func("/xml_dump/test_cr_Package_contains_forbidden_control_chars_02",
+                    test_cr_Package_contains_forbidden_control_chars_02);
+    g_test_add_func("/xml_dump/test_cr_Package_contains_forbidden_control_chars_03",
+                    test_cr_Package_contains_forbidden_control_chars_03);
+    g_test_add_func("/xml_dump/test_cr_Package_contains_forbidden_control_chars_04",
+                    test_cr_Package_contains_forbidden_control_chars_04);
+    g_test_add_func("/xml_dump/test_cr_Package_contains_forbidden_control_chars_05",
+                    test_cr_Package_contains_forbidden_control_chars_05);
+    g_test_add_func("/xml_dump/test_cr_GSList_of_cr_Dependency_contains_forbidden_control_chars_01",
+                    test_cr_GSList_of_cr_Dependency_contains_forbidden_control_chars_01);
+    g_test_add_func("/xml_dump/test_cr_GSList_of_cr_Dependency_contains_forbidden_control_chars_02",
+                    test_cr_GSList_of_cr_Dependency_contains_forbidden_control_chars_02);
     return g_test_run();
 }

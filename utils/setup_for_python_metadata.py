9d1c3ffd166d40b25ca6c8209759d1ecdf6d6090
Aleš Matěj <amatej@redhat.com>
2019-06-13 09:31:57 +0200

    Add missing python metadata to python2/3-createrepo_c (RhBug:1695677)
    In order not to depend on scikit-build I added additional python setup
    script (setup_for_python_metadata.py) which uses just setuptools and is
    used to generate egg-info metadata during building of packages
    python2/3-createrepo_c.

---
+++ utils/setup_for_python_metadata.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import sys
+
+# This is a simple and fragile way of passing the current version
+# from cmake to setup as I assume no one else will use this.
+#
+# This script has to have the version always specified as last argument.
+version = sys.argv.pop()
+
+setup(
+    name='createrepo_c',
+    description='C implementation of createrepo',
+    version=version,
+    license='GPLv2+',
+    author='RPM Software Management',
+    author_email='rpm-ecosystem@lists.rpm.org',
+    url='https://github.com/rpm-software-management',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Systems Administration',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

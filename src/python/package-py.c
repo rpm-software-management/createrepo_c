f2897176bc4590c53b277dabb7a1b06c9ebc5896
Aleš Matěj <amatej@redhat.com>
2020-02-27 09:08:49 +0100

    Convert PyTypeObject initializers to a more succinct and robust version

--- src/python/package-py.c
+++ src/python/package-py.c
@@ -583,51 +583,16 @@ static PyGetSetDef package_getsetters[] = {
 
 PyTypeObject Package_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    "createrepo_c.Package",         /* tp_name */
-    sizeof(_PackageObject),         /* tp_basicsize */
-    0,                              /* tp_itemsize */
-    (destructor) package_dealloc,   /* tp_dealloc */
-    0,                              /* tp_print */
-    0,                              /* tp_getattr */
-    0,                              /* tp_setattr */
-    0,                              /* tp_compare */
-    (reprfunc) package_repr,        /* tp_repr */
-    0,                              /* tp_as_number */
-    0,                              /* tp_as_sequence */
-    0,                              /* tp_as_mapping */
-    0,                              /* tp_hash */
-    0,                              /* tp_call */
-    (reprfunc)package_str,          /* tp_str */
-    0,                              /* tp_getattro */
-    0,                              /* tp_setattro */
-    0,                              /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_BASETYPE, /* tp_flags */
-    package_init__doc__,            /* tp_doc */
-    0,                              /* tp_traverse */
-    0,                              /* tp_clear */
-    0,                              /* tp_richcompare */
-    0,                              /* tp_weaklistoffset */
-    PyObject_SelfIter,              /* tp_iter */
-    0,                              /* tp_iternext */
-    package_methods,                /* tp_methods */
-    0,                              /* tp_members */
-    package_getsetters,             /* tp_getset */
-    0,                              /* tp_base */
-    0,                              /* tp_dict */
-    0,                              /* tp_descr_get */
-    0,                              /* tp_descr_set */
-    0,                              /* tp_dictoffset */
-    (initproc) package_init,        /* tp_init */
-    0,                              /* tp_alloc */
-    package_new,                    /* tp_new */
-    0,                              /* tp_free */
-    0,                              /* tp_is_gc */
-    0,                              /* tp_bases */
-    0,                              /* tp_mro */
-    0,                              /* tp_cache */
-    0,                              /* tp_subclasses */
-    0,                              /* tp_weaklist */
-    0,                              /* tp_del */
-    0,                              /* tp_version_tag */
-    0,                              /* tp_finalize */
+    .tp_name = "createrepo_c.Package",
+    .tp_basicsize = sizeof(_PackageObject),
+    .tp_dealloc = (destructor) package_dealloc,
+    .tp_repr = (reprfunc) package_repr,
+    .tp_str = (reprfunc)package_str,
+    .tp_flags = Py_TPFLAGS_DEFAULT|Py_TPFLAGS_BASETYPE,
+    .tp_doc = package_init__doc__,
+    .tp_iter = PyObject_SelfIter,
+    .tp_methods = package_methods,
+    .tp_getset = package_getsetters,
+    .tp_init = (initproc) package_init,
+    .tp_new = package_new,
 };

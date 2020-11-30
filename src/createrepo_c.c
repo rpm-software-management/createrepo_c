44d978049dc27c36d4b79114a0f830434f774f95
Aleš Matěj <amatej@redhat.com>
2020-11-30 10:54:02 +0100

    Fix type warning, used GSList stores char pointers as void pointers

--- src/createrepo_c.c
+++ src/createrepo_c.c
@@ -842,7 +842,7 @@ main(int argc, char **argv)
                                                                      &failures,
                                                                      &tmp_err);
             if (!result) {
-                g_critical("Could not update module index from file %s: %s", element->data,
+                g_critical("Could not update module index from file %s: %s", (char *) element->data,
                            (tmp_err ? tmp_err->message : "Unknown error"));
                 g_clear_error(&tmp_err);
                 g_clear_pointer(&moduleindex, g_object_unref);

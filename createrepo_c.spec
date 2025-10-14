rpm-software-management/createrepo_c

commit c673138669790c970e3147c0d3ec2504ea0b2425

spec: Consistently use CMake RPM macros
Fedora is going to move from cmake with make to cmake with ninja
<https://fedoraproject.org/wiki/Changes/CMake_ninja_default>.
That would break building an RPM package from our spec file because
the new %cmake macro won't produce Makefiles:

    + make -j8 'RPM_OPT_FLAGS=-O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -pipe -Wall -Wno-complain-wrong-lang -Werror=format-security -Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3 -Wp,-D_GLIBCXX_ASSERTIONS -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -fstack-protector-strong -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -m64 -march=x86-64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection -mtls-dialect=gnu2 -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer'
    make: *** No targets specified and no makefile found.  Stop.

The fix is to use CMake RPM macros for everything, especially not
executing make manually.

I tested this fix with old (make-based) and new (ninja-based)
cmake-rpm-macros package successfully.

Resolve: https://bugzilla.redhat.com/show_bug.cgi?id=2380983

createrepo_c.spec
@@ -145,25 +145,25 @@ pushd build-py3
       -DWITH_LEGACY_HASHES=%{?with_legacy_hashes:ON}%{!?with_legacy_hashes:OFF} \
       -DENABLE_DRPM=%{?with_drpm:ON}%{!?with_drpm:OFF} \
       -DWITH_SANITIZERS=%{?with_sanitizers:ON}%{!?with_sanitizers:OFF}
-  make %{?_smp_mflags} RPM_OPT_FLAGS="%{optflags}"
+  %cmake_build
   # Build C documentation
-  make doc-c
+  %cmake_build -t doc-c
 popd
 
 %check
 # Run Python 3 tests
 pushd build-py3
   # Compile C tests
-  make tests
+  %cmake_build -t tests
 
   # Run Python 3 tests
-  make ARGS="-V" test
+  %ctest -V
 popd
 
 %install
 pushd build-py3
   # Install createrepo_c with Python 3
-  make install DESTDIR=%{buildroot}
+  %cmake_install
 popd
 
 %if 0%{?fedora} || 0%{?rhel} > 7

commit 18aece2af03aa530a04bd5727cce21920894fa1f

spec: Work around a bug in ctest macro on RHEL 9
Building the RPM package on RHEL 9 failed like this:

    error: /builddir/build/originals/createrepo_c.spec: line 160: Unknown option V in ctest(:-:)

The failure was introduced with commit
c673138669790c970e3147c0d3ec2504ea0b2425 ("spec: Consistently use
CMake RPM macros"). The cause is a bug in a definition of the ctest
macro provided with cmake-rpm-macros-3.26.5-2.el9.noarch by RHEL 9
<https://issues.redhat.com/browse/RHEL-120543>.

This patch works around the bug by redefining the macro while accepting
any options.

createrepo_c.spec
@@ -157,6 +157,10 @@ pushd build-py3
   %cmake_build -t tests
 
   # Run Python 3 tests
+  %if 0%{?rhel} == 9 && %{defined ctest}
+    # Work around broken passing options to ctest macro, RHEL-120543
+    %global ctest(-) %{expand:%{macrobody:ctest}}
+  %endif
   %ctest -V
 popd
 
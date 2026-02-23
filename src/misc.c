rpm-software-management/createrepo_c

commit 9ff916d5fa738c73bd9ee796d67d29c65b4b7dfb

Remove uneeded inclusions of <rpm/rpmlib.h>
The header file is not actually needed to be included on matny places,
especially in public API createrepo_c header files as given files do
not use any RPM symbols.

src/misc.c
@@ -25,7 +25,7 @@
 #include <curl/curl.h>
 #include <errno.h>
 #include <ftw.h>
-#include <rpm/rpmlib.h>
+#include <rpm/rpmver.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

src/parsehdr.c
@@ -19,8 +19,12 @@
 
 #include <glib.h>
 #include <assert.h>
+#include <rpm/header.h>
 #include <rpm/rpmfi.h>
 #include <rpm/rpmpgp.h>
+#include <rpm/rpmtag.h>
+#include <rpm/rpmtd.h>
+#include <rpm/rpmver.h>
 #include <stdlib.h>
 #include "parsehdr.h"
 #include "xml_dump.h"

src/deltarpms.h.in
@@ -25,7 +25,6 @@ extern "C" {
 #endif
 
 #include <glib.h>
-#include <rpm/rpmlib.h>
 #include "package.h"
 #include "parsehdr.h"
 #include "xml_file.h"

src/dumper_thread.h
@@ -25,7 +25,6 @@ extern "C" {
 #endif
 
 #include <glib.h>
-#include <rpm/rpmlib.h>
 #include "load_metadata.h"
 #include "locate_metadata.h"
 #include "misc.h"

src/parsehdr.h
@@ -25,7 +25,7 @@ extern "C" {
 #endif
 
 #include <glib.h>
-#include <rpm/rpmlib.h>
+#include <rpm/rpmtypes.h>
 #include "package.h"
 
 /** \defgroup   parsehdr         Header parser API.

src/sqlite.c
@@ -26,6 +26,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <libxml/encoding.h>
 #include <libxml/xmlstring.h>

commit b83e1581071aac4fbfb520619fc2c83178da467a

Restore compatibility with RPM 4.14.3
Commit 9ff916d5fa738c73bd9ee796d67d29c65b4b7dfb ("Remove uneeded
inclusions of <rpm/rpmlib.h>") broke comptability with RPM before
4.16.0-beta2. Building with 4.14.3 failed:

    /tmp/createrepo_c/src/misc.c:28:10: fatal error: rpm/rpmver.h: No such file or directory
     #include <rpm/rpmver.h>
              ^~~~~~~~~~~~~~
This patch restores a compatibility with older rpm versions. Tested
with rpm-4.14.3.

(In the future, createrepo_c should define a minimal supported RPM
version.)

Relates: #464

src/CMakeLists.txt
@@ -78,6 +78,11 @@ IF (NOT HAVE_NFTW)
     ENDIF ()
 ENDIF ()
 
+# rpm/rpmver.h exists since RPM 4.16.0-beta2.
+IF (RPM_VERSION VERSION_GREATER_EQUAL "4.16.0")
+    ADD_COMPILE_DEFINITIONS(HAVE_RPM_RPMVER_H)
+ENDIF ()
+
 IF (BUILD_LIBCREATEREPO_C_SHARED)
   SET (createrepo_c_library_type SHARED)
 ELSE ()

src/misc.c
@@ -25,7 +25,11 @@
 #include <curl/curl.h>
 #include <errno.h>
 #include <ftw.h>
+#ifdef HAVE_RPM_RPMVER_H
 #include <rpm/rpmver.h>
+#else
+#include <rpm/rpmlib.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

src/parsehdr.c
@@ -24,7 +24,11 @@
 #include <rpm/rpmpgp.h>
 #include <rpm/rpmtag.h>
 #include <rpm/rpmtd.h>
+#ifdef HAVE_RPM_RPMVER_H
 #include <rpm/rpmver.h>
+#else
+#include <rpm/rpmlib.h>
+#endif
 #include <stdlib.h>
 #include "parsehdr.h"
 #include "xml_dump.h"